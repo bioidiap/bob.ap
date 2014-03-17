484bf14760e181cbfd3211d027453860c25c775a
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-17 09:14:02 +0100

    Fix python3 compatibility issues with doc fix

--- xbob/ap/__init__.py
+++ xbob/ap/__init__.py
@@ -2,5 +2,4 @@ from ._library import __version__
 from ._library import *
 
 # gets sphinx autodoc done right - don't remove it
-__all__ = [k for k in dir() if not k.startswith('_')]
-del k
+__all__ = [_ for _ in dir() if not _.startswith('_')]
