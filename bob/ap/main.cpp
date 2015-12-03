234d80e81a27885529ebed6d5fe841c7974a5975
Manuel Gunther <siebenkopf@googlemail.com>
2015-12-03 11:14:59 -0700

    Corrected error cases in module creation

--- bob/ap/main.cpp
+++ bob/ap/main.cpp
@@ -48,11 +48,13 @@ static PyObject* create_module (void) {
 
 # if PY_VERSION_HEX >= 0x03000000
   PyObject* m = PyModule_Create(&module_definition);
+  auto m_ = make_xsafe(m);
+  const char* ret = "O";
 # else
   PyObject* m = Py_InitModule3(BOB_EXT_MODULE_NAME, module_methods, module_docstr);
+  const char* ret = "N";
 # endif
   if (!m) return 0;
-  auto m_ = make_safe(m); ///< protects against early returns
 
   /* register the types to python */
   Py_INCREF(&PyBobApFrameExtractor_Type);
@@ -72,7 +74,7 @@ static PyObject* create_module (void) {
   if (import_bob_core_logging() < 0) return 0;
   if (import_bob_sp() < 0) return 0;
 
-  return Py_BuildValue("O", m);
+  return Py_BuildValue(ret, m);
 }
 
 PyMODINIT_FUNC BOB_EXT_ENTRY_NAME (void) {
